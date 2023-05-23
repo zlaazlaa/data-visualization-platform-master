669673ab2a7ded24631be407618b6da48ec811d3
zlaa <2889827787@qq.com>
2023-05-24 01:13:19 +0800

    修复bug

--- src/views/my_predict.vue
+++ src/views/my_predict.vue
@@ -1,47 +1,47 @@
 <template>
-    <div>
-      <input type="file" @change="handleFileUpload" accept=".csv">
-      <button @click="uploadFile">Upload</button>
-      <div v-if="resultData">
-        <p>Download Link:</p>
-        <a :href="resultData" download>Download Result</a>
-      </div>
+  <div>
+    <input type="file" @change="handleFileUpload" accept=".csv">
+    <button @click="uploadFile">Upload</button>
+    <div v-if="resultData">
+      <p>Download Link:</p>
+      <a :href="resultData" download>Download Result</a>
     </div>
-  </template>
+  </div>
+</template>
   
-  <script>
-  export default {
-    data() {
-      return {
-        selectedFile: null,
-        resultData: null
-      };
+<script>
+export default {
+  data() {
+    return {
+      selectedFile: null,
+      resultData: null
+    };
+  },
+  methods: {
+    handleFileUpload(event) {
+      this.selectedFile = event.target.files[0];
     },
-    methods: {
-      handleFileUpload(event) {
-        this.selectedFile = event.target.files[0];
-      },
-      uploadFile() {
-        const formData = new FormData();
-        formData.append("file", this.selectedFile);
-  
-        fetch("https://database-experiment-flask.azurewebsites.net/upload", {
-          method: "POST",
-          body: formData
+    uploadFile() {
+      const formData = new FormData();
+      formData.append("file", this.selectedFile);
+
+      fetch("https://database-experiment-flask.azurewebsites.net/upload", {
+        method: "POST",
+        body: formData
+      })
+        .then(response => response.json())
+        .then(data => {
+          if (data.code === 200) {
+            this.resultData = "https://database-experiment-flask.azurewebsites.net/download/" + data.data;
+          } else {
+            console.error(data.data);
+          }
         })
-          .then(response => response.json())
-          .then(data => {
-            if (data.code === 200) {
-              this.resultData = "https://database-experiment-flask.azurewebsites.net/download/" + data.data;
-            } else {
-              console.error(data.data);
-            }
-          })
-          .catch(error => {
-            console.error(error);
-          });
-      }
+        .catch(error => {
+          console.error(error);
+        });
     }
-  };
-  </script>
+  }
+};
+</script>
   
